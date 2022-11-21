6ace13cb40d5ec4170a45524f7b6ac8aaf03edc5
Gabriel Henrique da Silva Ferreira <ghenrique_10@hotmail.com>
2022-11-21 14:17:12 -0300

    Power Shell + Oh My Posh settings updated.

--- Windows/PowerShell/profile.ps1
+++ Windows/PowerShell/profile.ps1
@@ -1,15 +1,17 @@
 # Powerline
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme "~\PoshThemes\nordagnosterplus.json"
+Set-PoshPrompt -Theme "~\PoshThemes\customagnosterplus.json"
 
 # Autocomplete
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-# Alias
+# Functions
 Function Clear-Branches { 
   git branch | Select-String -NotMatch -Pattern "\*[\w\d-\/]*" | ForEach-Object -Process { git branch -D $_.ToString().Trim() } 
 }
+
+# Alias
 Set-Alias -Name "gbpurge" -Value Clear-Branches
